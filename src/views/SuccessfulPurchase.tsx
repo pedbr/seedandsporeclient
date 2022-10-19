acbc079bc4cbe7ac58eaf781640c9b689295a67a
pedbr <pedro.c.bras@gmail.com>
2022-10-19 14:53:10 +0100

    adds better status handling on success page

--- src/views/SuccessfulPurchase.tsx
+++ src/views/SuccessfulPurchase.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import { useSearchParams } from 'react-router-dom'
 
 const SuccessfulPurchase = () => {
-  return <div>Congrats! Your purchase was completed successfully</div>
+  const [searchParams] = useSearchParams()
+
+  const paymentStatus = searchParams.get('redirect_status')
+
+  const getMessage = () => {
+    switch (paymentStatus) {
+      case 'succeeded':
+        return 'Payment succeeded!'
+
+      case 'processing':
+        return 'Your payment is processing.'
+
+      case 'requires_payment_method':
+        return 'Your payment was not successful, please try again.'
+
+      default:
+        return 'Something went wrong.'
+    }
+  }
+
+  return (
+    <Box>
+      <Typography variant={'h1'}>{getMessage()}</Typography>
+    </Box>
+  )
 }
 
 export default SuccessfulPurchase
